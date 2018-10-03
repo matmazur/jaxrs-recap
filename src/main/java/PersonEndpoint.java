e6bd0e8c54f172e416d922451a857d96d500bf3c
Bingo Bobo <matmazur90@gmail.com>
2018-10-03 18:31:22 +0200

    added additional and optional parameter in PersonEndpoint class

--- src/main/java/PersonEndpoint.java
+++ src/main/java/PersonEndpoint.java
@@ -1,18 +1,34 @@
 import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
-@Path("/people/{id}")
+@Path("/people")
 @RequestScoped
 public class PersonEndpoint {
 
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getPerson(@PathParam("id") Long id){
+    public List<String> getAll(@QueryParam("orderBy") @DefaultValue("asc") String order) {
+        List<String> people = new ArrayList<>(Arrays.asList("Person 1", "Person 2"));
+
+        if (order.equals("asc")) {
+            people.sort(String::compareTo);
+        } else {
+            people.sort(Comparator.reverseOrder());
+        }
+        return people;
+    }
+
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getPerson(@PathParam("id") Long id) {
 
         return "Person " + id;
     }
