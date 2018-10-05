bde18ccee4a8ddbf18784740b9a9608f6f768ed1
Bingo Bobo <matmazur90@gmail.com>
2018-10-05 19:45:42 +0200

    finished class User, added persistence.xml

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,20 +2,20 @@ package model;
 
 import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Objects;
 
 @Entity
 @NamedQueries({
-        @NamedQuery(name = "User.findAll", query ="SELECT u FROM User u"),
-        @NamedQuery(name = "User.findById",query = "SELECT u from User u WHERE u")
+        @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u"),
+        @NamedQuery(name = "User.findById", query = "SELECT u from User u WHERE u.id = :id")
 })
 @XmlRootElement
 public class User {
 
 
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
     private String firstName;
     private String lastName;
@@ -36,4 +36,82 @@ public class User {
     public User() {
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(telephone, user.telephone) &&
+                Objects.equals(pesel, user.pesel) &&
+                Objects.equals(details, user.details);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", pesel='" + pesel + '\'' +
+                ", details=" + details +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, telephone, pesel, details);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public UserDetails getDetails() {
+        return details;
+    }
+
+    public void setDetails(UserDetails details) {
+        this.details = details;
+    }
 }
