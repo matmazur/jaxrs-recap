a12857137b67c7ee0b00f3fb8425db51539ebfec
Bingo Bobo <matmazur90@gmail.com>
2018-10-09 15:54:22 +0200

    slight changes in Mapper class to operate on violations

--- src/main/java/validatorsJaxRS/ViolationExceptionMapper.java
+++ src/main/java/validatorsJaxRS/ViolationExceptionMapper.java
@@ -1,15 +1,20 @@
 package validatorsJaxRS;
 
+import org.jboss.resteasy.api.validation.ResteasyViolationException;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import javax.xml.bind.ValidationException;
 
 @Provider
-public class ViolationExceptionMapper implements ExceptionMapper<ValidationException> {
+public class ViolationExceptionMapper implements ExceptionMapper<ResteasyViolationException> {
 
     @Override
-    public Response toResponse(ValidationException e) {
+    public Response toResponse(ResteasyViolationException e) {
+
+        System.out.println(e.getViolations());
         return Response.status(Response.Status.BAD_REQUEST).entity("Bad request").build();
+
+
     }
 }
