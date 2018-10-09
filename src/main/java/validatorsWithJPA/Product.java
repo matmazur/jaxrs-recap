b4bde54ff3232aaba2ee758e2ae858f8642c55fc
Bingo Bobo <matmazur90@gmail.com>
2018-10-09 13:04:15 +0200

    created new package for quick jpa validators check, added class Product as model POJO

---
+++ src/main/java/validatorsWithJPA/Product.java
@@ -0,0 +1,75 @@
+package validatorsWithJPA;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Size(min = 2)
+    private String name;
+    @NotBlank
+    private String description;
+    @Min(0)
+    private Integer quantity;
+
+
+    public Product(String name, String description, Integer quantity) {
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public Product() {
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
